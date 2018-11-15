2334fe767006791d4641a8e5ce8632161dba73cf
Tiffany Carter <tafby88@gmail.com>
2018-11-15 18:28:42 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,12 @@
-# Code your solution here!
+def run_guessing_game
+  while command != "exit"
+    puts "Guess a number!"
+  command = gets.chomp
+  number = rand(1..10).to_s
+  if command == number
+    puts "You guessed the correct number!"
+  else 
+    break
+  end
+  end
+end
