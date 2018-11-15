671d07a8a4d7b3fc3bec98330e342049c9175d55
Tiffany Carter <tafby88@gmail.com>
2018-11-15 18:30:29 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   command = nil
   number = nil
   while command != "exit"
-    puts "Guess a number!"
+    puts "Guess a number between 1 and 6!"
       command = gets.chomp
       number = rand(1..6).to_s
   if command == number
